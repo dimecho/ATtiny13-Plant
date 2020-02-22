b54ef137772c93209d75402e6b5c95d2aafce10c
Dima <dima@Workgroup.local>
2020-02-21 18:26:44 -0800

    better cert signing

--- gui/Windows/Drivers/selfsign.ps1
+++ gui/Windows/Drivers/selfsign.ps1
@@ -1,15 +1,16 @@
-$cert = New-SelfSignedCertificate -Subject "ATtiny13 Plant" -Type CodeSigningCert -NotAfter (Get-Date).AddMonths(60) -CertStoreLocation cert:\LocalMachine\My
-$CertPassword = ConvertTo-SecureString -String "P@ss0wrd" -Force -AsPlainText
-Export-PfxCertificate -Cert $cert -FilePath $PSScriptRoot\usbtiny.pfx -Password $CertPassword
+$cert = Get-ChildItem cert:\CurrentUser\My | where { $_.Subject -eq "CN=ATtiny13 Plant" }
 
-..\selfsign\inf2cat.exe /driver:$PSScriptRoot /os:10_X64 /verbose
+if ($cert.length -eq 0)
+{
+    $cert = New-SelfSignedCertificate -Subject "ATtiny13 Plant" -Type CodeSigningCert -NotAfter (Get-Date).AddMonths(60) -CertStoreLocation cert:\CurrentUser\My
+}
 
-..\selfsign\signtool.exe sign /f $PSScriptRoot\usbtiny.pfx /p P@ss0wrd /t http://timestamp.verisign.com/scripts/timstamp.dll /v $PSScriptRoot\usbtiny.cat
-
-..\selfsign\signtool.exe verify /v /pa $PSScriptRoot\usbtiny.cat
-
-$cert = Get-ChildItem cert:\LocalMachine\My | where { $_.Subject -eq "CN=ATtiny13 Plant" }
 Export-Certificate -Cert $cert -FilePath $PSScriptRoot\usbtiny.cer
 
-Import-Certificate -FilePath $PSScriptRoot\usbtiny.cer -CertStoreLocation cert:\LocalMachine\Root
-Import-Certificate -FilePath $PSScriptRoot\usbtiny.cer -CertStoreLocation cert:\LocalMachine\TrustedPublisher
+#inf2cat.exe /driver:$PSScriptRoot /os:10_X64 /verbose
+
+Set-AuthenticodeSignature $PSScriptRoot\usbtiny.cat -Certificate $cert -TimeStampServer "http://timestamp.verisign.com/scripts/timstamp.dll"
+Get-AuthenticodeSignature $PSScriptRoot\usbtiny.cat -Verbose | fl
+
+#Import-Certificate -FilePath $PSScriptRoot\usbtiny.cer -CertStoreLocation cert:\CurrentUser\Root
+#Import-Certificate -FilePath $PSScriptRoot\usbtiny.cer -CertStoreLocation cert:\CurrentUser\TrustedPublisher
